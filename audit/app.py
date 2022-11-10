5829ae3295f13537796bbbeb902c3df8345c2c6d
rj <rjgayem@gmail.com>
2022-11-10 10:55:27 -0800

    added cors

--- audit/app.py
+++ audit/app.py
@@ -9,6 +9,7 @@ import yaml
 import logging, logging.config
 from pykafka import KafkaClient
 from pykafka.common import OffsetType
+from flask_cors import CORS, cross_origin
 
 with open("app_conf.yml", "r") as f:
     app_config = yaml.safe_load(f.read())
@@ -81,6 +82,8 @@ def get_search_item(index):
     return { "message": "Not Found"}, 404
 
 app = connexion.FlaskApp(__name__, specification_dir="")
+CORS(app.app)
+app.app.config['CORS_HEADERS'] = 'Content-Type'
 app.add_api("openapi.yaml", strict_validation=True, validate_responses=True)
 if __name__ == "__main__":
     app.run(port=8110)
