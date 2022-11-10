48ac89268cac0c56c33884ccb7472e71eb68e266
rj <rjgayem@gmail.com>
2022-11-09 21:20:31 -0800

    remove print

--- processing/app.py
+++ processing/app.py
@@ -68,7 +68,7 @@ def populate_stats():
         buy_data = res_buy.json()
         buy_price = []
        
-        print(buy_data)
+        
         for item in buy_data:
             buy_price.append(float(item['price']))
         
@@ -90,8 +90,8 @@ def populate_stats():
         )
         
         session.add(bs)
-        session.commit()
-        session.close()
+    session.commit()
+    session.close()
     return NoContent, 201
 
 def init_scheduler():
