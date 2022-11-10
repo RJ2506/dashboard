48092ae1e16afc340df66554e936f480312ae283
rj <rjgayem@gmail.com>
2022-11-09 19:29:42 -0800

    updated audit and processing

--- processing/app.py
+++ processing/app.py
@@ -13,6 +13,8 @@ import datetime
 import apscheduler
 from apscheduler.schedulers.background import BackgroundScheduler
 
+from flask_cors import CORS, cross_origin
+
 
 
 
@@ -102,6 +104,8 @@ def init_scheduler():
     sched.start()
 
 app = connexion.FlaskApp(__name__, specification_dir="")
+CORS(app.app)
+app.app.config['CORS_HEADERS'] = 'Content-Type'
 app.add_api("openapi.yml", strict_validation=True, validate_responses=True)
 
 if __name__ == "__main__":
