e40c4a0dd4863369d244c1d0f4d01bb392224b62
rj <rjgayem@gmail.com>
2022-11-10 18:53:45 -0800

    made the indexes sync

--- dashboard-ui/src/components/EndpointAudit.js
+++ dashboard-ui/src/components/EndpointAudit.js
@@ -6,33 +6,34 @@ export default function EndpointAudit(props) {
     const [log, setLog] = useState(null);
     const [error, setError] = useState(null)
 	const rand_val = Math.floor(Math.random() * 100); // Get a random event from the event store
-
+    const [index, setIndex] = useState(null)
     const getAudit = () => {
         fetch(`http://acit3855-kafka-lab6a.eastus.cloudapp.azure.com:8110/${props.endpoint}?index=${rand_val}`)
-            .then(res => res.json())
-            .then((result)=>{
-				console.log("Received Audit Results for " + props.endpoint)
-                setLog(result);
-                setIsLoaded(true);
-            },(error) =>{
-                setError(error)
-                setIsLoaded(true);
-            })
+        .then(res => res.json())
+        .then((result)=>{
+            console.log("Received Audit Results for " + props.endpoint)
+            setLog(result);
+            setIsLoaded(true);
+        },(error) =>{
+            setError(error)
+            setIsLoaded(true);
+        })
     }
 	useEffect(() => {
-		const interval = setInterval(() => getAudit(), 4000); // Update every 4 seconds
+        const interval = setInterval(() => getAudit(), 4000); // Update every 4 seconds
 		return() => clearInterval(interval);
     }, [getAudit]);
-
+    
     if (error){
         return (<div className={"error"}>Error found when fetching from API</div>)
     } else if (isLoaded === false){
         return(<div>Loading...</div>)
     } else if (isLoaded === true){
         
+        setIndex(rand_val);
         return (
             <div>
-                <h3>{props.endpoint}-{rand_val}</h3>
+                <h3>{props.endpoint}-{index}</h3>
                 {JSON.stringify(log)}
             </div>
         )
